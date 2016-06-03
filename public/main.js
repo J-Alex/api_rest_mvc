055a5a24e12f124529d2eba7815b2a44aca1997d
J-Alex <jlex.wd@outlook.com>
2016-06-02 21:59:35 -0600

    Socket IO

--- public/main.js
+++ public/main.js
@@ -6,12 +6,12 @@
 
 	apl.controller("mainController",['$scope','$http', function(s,h){
 		s.formData = {};
-
+		s.todos = [];
 		//Cuando se cargue la página, pide del API todas las tareas
 		h.get('/api/todos')
 			.success(function(data) {
 				s.todos = data;
-				console.log(data);
+				//console.log(data);
 			})
 			.error(function(data) {
 				console.log('Error' + data);
@@ -19,7 +19,9 @@
 		
 		//cunado se añada una nueva tarea, manda el texto a la API
 		s.createTodo = function() {
-			h.post('/api/todos', s.formData)
+			socket.emit('saveOne',s.formData);
+			//s.todos = '';
+			/*h.post('/api/todos', s.formData)
 			.success(function(data) {
 				s.formData = {};
 				s.todos = data;
@@ -27,9 +29,17 @@
 			})
 			.error(function(data) {
 				console.log('Error' + data);
-			});
+			});*/
+			//getElementsByTagName('form')[0].reset();
 		}
 
+		socket.on('sendAll',function(data){
+			//console.log(data);
+			//s.todos = [];
+			s.todos.push(data[data.length-1]);
+			s.$apply();
+			console.log(s.todos);
+		});
 		//Borrar una tarea despues de checkearlo como acabado
 		s.deleteTodo = function(id) {
 			h.delete('/api/todos/' + id)
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var express = require('express'),
 
 //conexión a la base de datos
 mongoose.connect('mongodb://localhost:27017/angular-todo');
+var Todo = require('./models n schemas/todoModel').Todo;
 
 //configuración general del servidor
 //app.configure(function() {
@@ -84,7 +85,42 @@ app.get('*', function(req,res) {
 *************************************************************/
 var index = require(__dirname + '/controllers n routes/index.js');
 app.use('/', index);
-//escucha en el puerto 8000 y levanta el servidor
-app.listen(8000, function () {
-	console.log('Servidor arriba en el puerto 8000');
+
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+io.on('connection',function(socket){
+	socket.on('saveOne',function(dataIn){
+		//console.log(dataIn);
+		Todo.create({
+			text: dataIn.text,
+			done: false
+		}, function (err, todo) {
+				if(err){
+					console.log(err);
+				}
+				Todo.find(function(err, todos) {
+					if(err){
+						console.log(err);
+					}	
+					//console.log(todos);		
+					socket.emit('sendAll', todos);		
+					socket.broadcast.emit('sendAll', todos);//res.json(todos);
+				});
+			});
+	});
 });
+io.on('connection', function(socket){
+  socket.on('mensage', function(msg){
+    console.log('message: ' + msg.saludo);
+	socket.emit('retorno',{devuelta:"Hola"});
+  });
+});
+
+http.listen(8080, function(){
+  console.log('listening on localhost:8080');
+});
+//escucha en el puerto 8000 y levanta el servidor
+/*app.listen(8000, function () {
+	console.log('Servidor arriba en el puerto 8000');
+});*/
